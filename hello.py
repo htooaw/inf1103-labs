3f76ae68e27e818ff306123e37427114be90bbfa
Htoo Aung Win <htooaungwinhawhawhaw@gmail.com>
2026-09-04 10:23:36 +0800

    Follower Growth Tracker

--- hello.py
+++ hello.py
@@ -11,3 +11,15 @@ followers = 1000
 print("Username:", username)
 print("Bio:", bio) 
 print("Followers:", followers)
+
+followers = 100
+
+followers += 50
+print("Day1:",followers)
+
+followers += 20
+print("Day2:", followers)
+
+followers -= 10
+print("Day3:", followers)
+
