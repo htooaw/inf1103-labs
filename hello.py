8172f97fd35d87dd17dce85e9e78a47a08f23ceb
Htoo Aung Win <htooaungwinhawhawhaw@gmail.com>
2026-09-04 10:21:50 +0800

    Update your Profile and add it to the repository

--- hello.py
+++ hello.py
@@ -2,3 +2,12 @@ print("==============================")
 print("welcome here")
 print("my first post")
 print("==============================")
+
+
+username = "cool creator"
+bio = "Fun blogger"
+followers = 1000
+
+print("Username:", username)
+print("Bio:", bio) 
+print("Followers:", followers)
